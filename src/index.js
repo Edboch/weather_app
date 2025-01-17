91cc02c8c29746163e57069b127c2152b7dee88a
Edmond <edmondchu10@hotmail.com>
2025-01-17 18:35:06 +0000

    feat: Add function to fetch weather data by location

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import "./style.css"
 
+async function getLocationWeather(loc) {
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${loc}?unitGroup=uk&key=974CB98EEDPG3VEFFCX2KQUDW&contentType=json`;
+    const response = await fetch(url,{"method":"GET", "headers":{}});
+    const data = await response.json();
+    console.log(data);
+}
+
+getLocationWeather("JaPAN");
